0535b50b6df584725c889c9750a470d8ebe17a0b
zixun <lvzixun@gmail.com>
2015-04-01 13:36:14 +0800

    check filename

--- st_cc.py
+++ st_cc.py
@@ -239,11 +239,12 @@ class CCAutoComplete(sublime_plugin.EventListener):
 
     file_name = view.file_name()
 
-    if not can_complete(view):
+    if not can_complete(view) or filename==None:
       return
 
     #flag = sublime.INHIBIT_WORD_COMPLETIONS | sublime.INHIBIT_EXPLICIT_COMPLETIONS
     flag = 0
+    aa = 123
     if self.complete_result != None:
       ret = None
       ret = (self.complete_result, flag)
